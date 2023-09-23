92121925e08670a803230fbb8a78c43af7ed71ff
Devin W. Leaman <git@4lch4.email>
2023-09-23 15:12:55 -0500

    refactor: Update config schema to make 'img' and 'img_alt' optional, and add 'repo' field

--- astro/src/content/config.ts
+++ astro/src/content/config.ts
@@ -7,8 +7,9 @@ export const collections = {
 			description: z.string(),
 			publishDate: z.coerce.date(),
 			tags: z.array(z.string()),
-			img: z.string(),
+			img: z.string().optional(),
 			img_alt: z.string().optional(),
+			repo: z.string().optional(),
 		}),
 	}),
 	posts: defineCollection({
