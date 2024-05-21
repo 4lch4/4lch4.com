4d3a683a8e3a2c5ded0091d3c44f9f58532eb7a7
4lch4 <git@4lch4.email>
2024-05-20 20:08:22 -0500

    feat: added projects collection to config.ts

--- src/content/config.ts
+++ src/content/config.ts
@@ -12,6 +12,21 @@ export const collections = {
 			repo: z.string().optional(),
 		}),
 	}),
+
+	projects: defineCollection({
+		schema: z.object({
+			title: z.string().max(50),
+			description: z.string().max(75).optional(),
+			startDate: z.coerce.date(),
+			endDate: z.coerce.date().optional(),
+			status: z.enum(['Active', 'Maintenance', 'Archived']).nullable().optional(),
+			tags: z.array(z.string()),
+			img: z.string().optional(),
+			img_alt: z.string().optional(),
+			repo: z.string().optional(),
+		}),
+	}),
+
 	posts: defineCollection({
 		schema: z.object({
 			title: z.string(),
