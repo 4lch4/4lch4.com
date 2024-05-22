92b716c13257d9c1acf91a354a524ce3991f633e
4lch4 <git@4lch4.email>
2024-05-22 04:03:06 -0500

    refactor: added email address to `Constants.ts`

--- src/components/Constants.ts
+++ src/components/Constants.ts
@@ -1,2 +1,5 @@
 /** The public path to my avatar image. */
 export const AvatarPath = '/assets/img/Avatar-1.png'
+
+/** The email address used for the Contact Me buttons. */
+export const ContactEmail = 'homepage-cta@4lch4.email'
