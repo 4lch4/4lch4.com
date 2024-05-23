9dadd554e14e3b2990a7c76ff236c85de889f0e3
4lch4 <git@4lch4.email>
2024-05-23 18:51:39 -0500

    feat: added Vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
+import vercel from '@astrojs/vercel/serverless'
 import { defineConfig } from 'astro/config'
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://4lch4.com',
   integrations: [mdx(), sitemap()],
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: { enabled: true },
+  }),
 })
